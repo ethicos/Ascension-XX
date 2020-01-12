f713158c71d16ad38a5086bf5fb2af69bd04453c
Abhinav KM <abhinavkm184@gmail.com>
2020-01-13 00:22:14 +0530

    first build

---
+++ build/precache-manifest.b671fef92ae898ef2b2d2c2a7cba5526.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41470732e03fa3c7ee6ecd4528c17748",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b99394907a487731e08",
+    "url": "/static/css/main.4923aa5f.chunk.css"
+  },
+  {
+    "revision": "80c9e1961809dd27fcae",
+    "url": "/static/js/2.7d268d26.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7d268d26.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6b99394907a487731e08",
+    "url": "/static/js/main.3e8c9d39.chunk.js"
+  },
+  {
+    "revision": "367319f9b380230e78e6",
+    "url": "/static/js/runtime-main.db521de0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e22fa06256266c175342e5f2f4e7c32c",
+    "url": "/static/media/logo.e22fa062.png"
+  }
+]);
