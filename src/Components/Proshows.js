b7a5e6aed0d4a0933af84c1a0cc514462b601e85
Abhinav KM <abhinavkm184@gmail.com>
2020-01-21 00:59:31 +0530

    added rotation to proshow page

--- src/Components/Proshows.js
+++ src/Components/Proshows.js
@@ -1,13 +1,93 @@
 import React from 'react';
 import Show from './Show';
 
+import './assets/css/proshow.css';
+
 class Proshows extends React.Component {
     constructor(){
         super();
+        this.state = {
+            currentShowIndex: 0,
+            showIdList: ["show1", "show2", "show3", "show4"]
+        };
+        this.changeImage = this.changeImage.bind(this);
+    }
+    changeImage(dir){
+        const getIndex = (index)=>{
+            if( index >= this.state.showIdList.length )
+                return 0;
+            else if( index < 0)
+                return this.state.showIdList.length-1;
+            return index;
+        }
+        const addClass = (element, classNm)=>{
+            if(!element.classList.contains(classNm)){
+                element.classList.add(classNm);
+                return 1;
+            }
+            return 0;
+        }
+        const removeClass = (element, classNm)=>{
+            if(element.classList.contains(classNm)){
+                element.classList.remove(classNm);
+                console.log("removed class");
+            }
+        }
+        const removeAllClasses = (element, class1, class2)=>{ // classes to be exempted
+            for( let classnm of element.classList ){
+                if ( class1 == classnm || class2 == classnm )
+                    continue
+                else
+                    element.classList.remove(classnm);
+            }
+        }
+        const currentImage = document.getElementById( this.state.showIdList[this.state.currentShowIndex]);
+        if(dir==-1){
+            // rotate current image right
+            let x = addClass(currentImage, "rotateLF");
+            if(x){
+                addClass(currentImage, "rotateRB");
+            }
+            // rotate next image right
+            removeAllClasses(currentImage, "rotateLF", "show-img");
+        }
+        else if(dir==1){
+            // rotate current image left
+            let x = addClass(currentImage, "rotateLB");
+            if(x){
+                addClass(currentImage, "rotateRF");
+            }
+            
+            // rotate next image left
+            removeAllClasses(currentImage, "rotateLB", "show-img");
+        }
     }
     render(){
+        const showList = [
+            <Show title="Pro show 1" date="20 Feb" time="5 pm onwards" 
+                bookLink="http://google.com" showID={this.state.showIdList[0]} />,
+            <Show title="Pro show 2" date="21 Feb" time="6 pm onwards" 
+                bookLink="http://google.com" showID={this.state.showIdList[1]}/>,
+            <Show title="Pro show 3" date="22 Feb" time="4 pm onwards" 
+                bookLink="http://google.com" showID={this.state.showIdList[2]}/>,
+            <Show title="Pro show 4" date="23 Feb" time="7 pm onwards" 
+                bookLink="http://google.com" showID={this.state.showIdList[3]}/>
+        ];
+
         return (
-            <Show/>
+            
+            <div class="row">
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 column">
+                    <span className="prev-show" onClick={()=> this.changeImage(-1) }>Prev</span>   
+                </div>
+                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 showSection">
+                    {showList[0]}
+                </div>
+                <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 column">
+                    <span className="next-show" onClick={()=> this.changeImage(1) }>Next</span>   
+                </div>
+                
+            </div>
         );
     }
 }
--- src/Components/Show.js
+++ src/Components/Show.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import arch from './assets/images/gec-arch.png';
+import arch from './assets/images/arch.svg';
 
 import './assets/css/show.css';
 
@@ -11,10 +11,26 @@ class Show extends React.Component {
     render(){
         return (
             <div class="row show">
-                <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3"></div>
-                <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                    <img src={arch} className="show-img"/>
+                <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2"></div>
+                <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
+                    <img src={arch} className="show-img" id={this.props.showID}/>
+                    <h2 className="show-title">{this.props.title}</h2>
+
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                            <span className="show-date">{this.props.date}</span>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                            <span className="show-date">{this.props.time}</span>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                            <span className="show-book">
+                                <a href={this.props.bookLink}>Book Now</a>
+                            </span>
+                        </div>
+                    </div>
                 </div>
+                <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2"></div>
             </div>
         );
     }
