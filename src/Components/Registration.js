6514e5a874e3daa835bdc24e7736ca1248bb7afd
shaheemMPM <shaheem.mpm@gmail.com>
2020-01-22 09:34:20 +0530

    Add Modal For Logi

--- src/Components/Registration.js
+++ src/Components/Registration.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Registration = () => {
-    return ( 
-        <div>
-            <h2>Registration Page</h2>
-        </div>
-     );
+import Aux from './Aux';
+import Modal from './Modal';
+import Logo from './Logo';
+import './assets/css/Registration.css';
+
+class Registration extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+
+    modalClosedHandler = () => {
+        window.location.href = '/events';
+    }
+
+    render() { 
+        return ( 
+            <Aux>
+                <Modal show={true} modalClosed={this.modalClosedHandler}>
+                    <div className="ModalInner">
+                        <Logo size={0}/>
+                        <h3>Login to Dyuthi</h3>
+                        <span>Login to Dyuthi with your Google Account for Event Registration</span>
+                    </div>
+                </Modal>
+            </Aux>
+         );
+    }
 }
  
 export default Registration;
