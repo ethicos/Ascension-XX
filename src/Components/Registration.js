1a4a148a498d2ebcbd81f8e96b27178ad2f330f9
shaheemMPM <shaheem.mpm@gmail.com>
2020-01-25 14:14:17 +0530

    Create Login With Google: Working All devices

--- src/Components/Registration.js
+++ src/Components/Registration.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
 
+import firebase from 'firebase';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
+import fireconfig from '../config/firebase.config';
+
 import Aux from './Aux';
 import Modal from './Modal';
 import Logo from './Logo';
-import GoogleJoin from './GoogleJoin';
+// import GoogleJoin from './GoogleJoin';
 import './assets/css/Registration.css';
+
+firebase.initializeApp(fireconfig);
 
 class Registration extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            isSignedIn: false
+         }
+    }
+
+    uiConfig = {
+        signInFlow: "redirect",
+        signInOptions: [
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID
+        ],
+        callbacks: {
+          signInSuccess: () => false
+        }
+    }
+
+    componentDidMount = () => {
+        firebase.auth().onAuthStateChanged(user => {
+            this.setState({isSignedIn: !!user});
+        })
     }
 
     modalClosedHandler = () => {
@@ -19,12 +44,15 @@ class Registration extends Component {
     render() { 
         return ( 
             <Aux>
-                <Modal show={true} modalClosed={this.modalClosedHandler}>
+                <Modal show={!this.state.isSignedIn} modalClosed={this.modalClosedHandler}>
                     <div className="ModalInner">
                         <Logo size={0}/>
                         <h3>Login to Dyuthi</h3>
                         <span>Login to Dyuthi with your Google Account for Event Registration</span>
-                        <GoogleJoin />
+                        {/* <GoogleJoin /> */}
+                        <StyledFirebaseAuth 
+                            uiConfig={this.uiConfig}
+                            firebaseAuth={firebase.auth()}/>
                     </div>
                 </Modal>
             </Aux>
