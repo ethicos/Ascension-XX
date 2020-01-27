4f1de21b5489ed1114314ccd2b5912d8683cdc59
Abhinav KM <abhinavkm184@gmail.com>
2020-01-27 11:31:54 +0530

    prod build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0ec3926bc74eed3108b5bdcc3c804219.js"
+  "/precache-manifest.1137cbb4db0e6a169965baf255e33f99.js"
 );
 
 self.addEventListener('message', (event) => {
