ada07f543d65be63cb478a9213861645e17aa7b3
Abhinav KM <abhinavkm184@gmail.com>
2020-01-28 23:04:21 +0530

    updated with new events, prod build

---
+++ build/precache-manifest.2c63f4260e8af587b61eb8f204a689d0.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abbb01f04a414e6468db5970437f8a03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7703b35e22af1d8d17bc",
+    "url": "/static/css/main.ae65cee7.chunk.css"
+  },
+  {
+    "revision": "d10d84ade983e2b9eee4",
+    "url": "/static/js/2.aa09da00.chunk.js"
+  },
+  {
+    "revision": "1fb1b5fca2e9f4250ebcc3f3da15b17d",
+    "url": "/static/js/2.aa09da00.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7703b35e22af1d8d17bc",
+    "url": "/static/js/main.7793bfa8.chunk.js"
+  },
+  {
+    "revision": "367319f9b380230e78e6",
+    "url": "/static/js/runtime-main.db521de0.js"
+  },
+  {
+    "revision": "8c059b501b409a8cb6525f6991721d4d",
+    "url": "/static/media/3d.8c059b50.jpg"
+  },
+  {
+    "revision": "3ec228d3193537d0fb027a5f0a0a0a79",
+    "url": "/static/media/Google.3ec228d3.svg"
+  },
+  {
+    "revision": "7ba7d8fa5f04749a81a973c580326e2e",
+    "url": "/static/media/ai.7ba7d8fa.jpg"
+  },
+  {
+    "revision": "72c385cb646518c93b531fd05e8ff05e",
+    "url": "/static/media/anne amie.72c385cb.jpg"
+  },
+  {
+    "revision": "cd3dbfa7e54f4053f39dfb2b7c315e10",
+    "url": "/static/media/anne_amie.cd3dbfa7.jpeg"
+  },
+  {
+    "revision": "ac29a73efe46967c07e6bd732ba86a43",
+    "url": "/static/media/arch-cropped.ac29a73e.svg"
+  },
+  {
+    "revision": "0a39b4d5851e20dbe7d55642d5a9b628",
+    "url": "/static/media/arch.0a39b4d5.svg"
+  },
+  {
+    "revision": "e483b1e31fb02f49f51c6abdb395ae2d",
+    "url": "/static/media/cl1.e483b1e3.png"
+  },
+  {
+    "revision": "e483b1e31fb02f49f51c6abdb395ae2d",
+    "url": "/static/media/cl2.e483b1e3.png"
+  },
+  {
+    "revision": "3b75444a5ef2f95cbc68e332998890fb",
+    "url": "/static/media/cl3.3b75444a.png"
+  },
+  {
+    "revision": "3b75444a5ef2f95cbc68e332998890fb",
+    "url": "/static/media/cl4.3b75444a.png"
+  },
+  {
+    "revision": "af534476ec377ec214c49f8f0bb3fd66",
+    "url": "/static/media/events-active.af534476.png"
+  },
+  {
+    "revision": "7c3d998d89a06b56de30172c9cf42604",
+    "url": "/static/media/events.7c3d998d.png"
+  },
+  {
+    "revision": "72872958b543841b73d4e8417af3b4ba",
+    "url": "/static/media/full-gec.72872958.svg"
+  },
+  {
+    "revision": "a48b1d0692d6327c3f888e25384cbf85",
+    "url": "/static/media/gec-arch.a48b1d06.png"
+  },
+  {
+    "revision": "dc30d3fc2afe75f6123c000f673808ea",
+    "url": "/static/media/gec-night.dc30d3fc.png"
+  },
+  {
+    "revision": "3b20ee3f160c6f4e0c994f475cb58f74",
+    "url": "/static/media/gec.3b20ee3f.png"
+  },
+  {
+    "revision": "cc6e8beeabeb59720ecf80215db55c72",
+    "url": "/static/media/hacking-workshop.cc6e8bee.jpg"
+  },
+  {
+    "revision": "c02090d20295faffb63701e5032a9fb3",
+    "url": "/static/media/inst.c02090d2.jpg"
+  },
+  {
+    "revision": "54cc1e087d0248306a096e314051483c",
+    "url": "/static/media/julia-pc.54cc1e08.jpg"
+  },
+  {
+    "revision": "a6c26501eb9aec228e5fed047b9fc7b2",
+    "url": "/static/media/lagori-pc.a6c26501.jpg"
+  },
+  {
+    "revision": "f3235b138ab7455285b526dacf968add",
+    "url": "/static/media/lagori.f3235b13.jpg"
+  },
+  {
+    "revision": "e22fa06256266c175342e5f2f4e7c32c",
+    "url": "/static/media/logo.e22fa062.png"
+  },
+  {
+    "revision": "20cb5d2a652507bd3a2de737665c7bac",
+    "url": "/static/media/proshow.20cb5d2a.png"
+  },
+  {
+    "revision": "4ab59bf6437538d0d99264293ef3c479",
+    "url": "/static/media/snapchat.4ab59bf6.png"
+  },
+  {
+    "revision": "be61e7586f5e34a64ad51af1318af234",
+    "url": "/static/media/sooraj santhosh.be61e758.jpg"
+  },
+  {
+    "revision": "87038ccaa91ac497b7ac3cead5cf8564",
+    "url": "/static/media/ss dyuthi.87038cca.jpg"
+  },
+  {
+    "revision": "94e33f36470650aedcd04eb2f61946aa",
+    "url": "/static/media/swatch.94e33f36.ttf"
+  },
+  {
+    "revision": "3cfa81dd3e914a85edcbcb8f8fe80543",
+    "url": "/static/media/yello.3cfa81dd.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1137cbb4db0e6a169965baf255e33f99.js"
+  "/precache-manifest.2c63f4260e8af587b61eb8f204a689d0.js"
 );
 
 self.addEventListener('message', (event) => {
